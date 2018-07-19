617edc302e597f9354da2b2ef3a280cb4a1f87a7
gaojian <qiaogaojian@vip.qq.com>
2018-07-19 09:21:59 +0800

    格式化代码

--- bool/02PHPbasic/php11.php
+++ bool/02PHPbasic/php11.php
@@ -2,13 +2,13 @@
 $tittle = $_POST["tittle"];
 $content = $_POST["content"];
 echo ($_POST);
-echo "Tittle:".$tittle."<br>"."Content:".$content;
+echo "Tittle:" . $tittle . "<br>" . "Content:" . $content;
 
 //php打开文件 $fh是资源类型的变量  
-$fh = fopen("./msg.txt","a");
+$fh = fopen("./msg.txt", "a");
 
 //往文件写内容
-fwrite($fh,"Tittle:".$tittle."<br>"."Content:".$content."<br>");
+fwrite($fh, "Tittle:" . $tittle . "<br>" . "Content:" . $content . "<br>");
 
 fclose($fh);
 
