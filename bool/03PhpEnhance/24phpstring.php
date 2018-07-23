87d891bfe63f5a53e749f9fca9be0290bc175b93
Michael <qiaogaojian@vip.qq.com>
2018-07-24 00:25:16 +0800

    here doc  now doc  和 " "  ' '

--- bool/03PhpEnhance/24phpstring.php
+++ bool/03PhpEnhance/24phpstring.php
@@ -1,17 +1,27 @@
 <?php
 
-// 定义大段文本 headdoc nowdoc
-$str3 = <<<HTML
+// 定义大段文本 heredoc(" ") nowdoc(' ')
+$age = 18;
+
+$str3 = <<<DOC
 'Hello wor'ld' adb
 michael
+" \" \\ \n \t \v \$ $age";
 "ok" now;'
-HTML;
+DOC;
+
+$str4 = <<<'DOC'
+'Hello wor'ld' adb
+michael
+" \" \\ \n \t \v \$ $age";
+"ok" now;'
+DOC;
 
 echo $str3;
 echo "<br>";
-
+echo $str4;
+echo "<br>";
 // 单引号和双引号的区别 
-$age = 18;
 
 $str1 = " \" \\ \n \t \v \$ $age";
 echo $str1;
