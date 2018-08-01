5dfcfe36e9fc192f506533d657763d26ee6d4f34
Michael <qiaogaojian@vip.qq.com>
2018-08-01 23:34:32 +0800

    php递归学习

--- bool/03PhpEnhance/38phpdigui.php
+++ bool/03PhpEnhance/38phpdigui.php
@@ -9,7 +9,66 @@ function AddAdd($n)
     return AddAdd($n-1)+$n;
 }
 
-echo AddAdd(100);
+echo AddAdd(100),"<br>";
 
+$rootPath = ".";
 
+function DisplayDir($path,$level)
+{
+    $pathInfo = opendir($path);
+    if($pathInfo!==false)
+    {
+        while(($item = readdir($pathInfo))!==false)
+        {
+            echo str_repeat("----",$level),$item,"<br>";
+
+            if($item=='.'||$item=='..')
+            {
+                continue;
+            }
+            if(is_dir($path."/".$item))
+            {
+                DisplayDir($path."/".$item,$level+1);
+            }          
+        }
+    }
+    else
+    {
+        echo "无法读取目录.";
+        exit();
+    }
+    closedir($pathInfo);
+}
+
+DisplayDir($rootPath,1);
+
+function StaticR()
+{
+    static $a = 0;
+    $a++;
+    echo $a,"<br>";
+}
+
+StaticR();
+StaticR();
+StaticR();
+
+$arra = array(1,2,3,array(4,5,6));
+function Sum($arr)
+{
+    static $sumArr = 0;
+    foreach($arr as $v)
+    {
+        if(is_array($v))
+        {
+            Sum($v);
+        }
+        else
+        {
+            $sumArr += $v;
+        }
+    }
+    return $sumArr;
+}
+echo Sum($arra);
 ?>
---
+++ bool/03PhpEnhance/39phpdiguipractice.php
@@ -0,0 +1,24 @@
+<?php
+// 1 多维数组变2倍
+$array = array(1,2,'a',array(3,'b',array(4,5)));
+
+function DoubleArrayNum($arr)
+{    
+    foreach($arr as $k=>$v)
+    {
+        if(is_numeric($v))
+        {
+            $arr[$k] = $v*2;
+        }
+        elseif(is_array($v))
+        {
+            $arr[$k] = DoubleArrayNum($v);
+        }
+    }
+    return $arr;
+}
+
+print_r(DoubleArrayNum($array));
+
+
+?>
