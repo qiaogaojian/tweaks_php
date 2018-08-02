ffdd9320e3468fd3909f56f24db05c5679ec5ffb
Michael <qiaogaojian@vip.qq.com>
2018-08-03 00:42:37 +0800

    留言本数据源转移到MySql数据库

--- bool/02PHPbasic/php12MessageBoard.php
+++ bool/02PHPbasic/php12MessageBoard.php
@@ -31,15 +31,52 @@
         <div id="down">     
            
             <?php
-            $fileMessage = file("msg.txt");
-            if (count($fileMessage) > 0) {
-                foreach ($fileMessage as $message) {
-                    $messageText = json_decode($message);
-                    echo "<div class='board'> PostTime: " . $messageText->time . "<br>";
-                    echo "Tittle: " . $messageText->tittle . "<br>";
-                    echo "Content:<br>" . $messageText->content . "<br></div>";
+
+            //MySql数据库读取法
+            $servername = "127.0.0.1";
+            $username = "root";
+            $password = "root";
+            $dbname = "messageboard";
+            $tbname = "message";
+
+            $dbConnect = mysqli_connect($servername, $username, $password);
+
+            if (!$dbConnect) {
+                echo "无法连接数据库." . mysqli_error();
+                exit();
+            }
+            
+            mysqli_select_db($dbConnect,$dbname);
+            
+            $sql = "select * from $tbname";
+            $res = mysqli_query($dbConnect, $sql);
+
+            if($res)
+            {
+                while(($row = mysqli_fetch_assoc($res)))
+                {               
+                    echo "<div class='board'> PostTime: " , $row['time'] , "<br>";
+                    echo "Tittle: " . $row ['tittle'] . "<br>";
+                    echo "Content:<br>" . $row['content'] . "<br></div>";
                 }
             }
+            else
+            {
+                echo "数据库查询失败.";
+            }          
+            
+            mysqli_close($dbConnect);
+
+            //文件读取法
+            // $fileMessage = file("msg.txt");
+            // if (count($fileMessage) > 0) {
+            //     foreach ($fileMessage as $message) {
+            //         $messageText = json_decode($message);
+            //         echo "<div class='board'> PostTime: " . $messageText->time . "<br>";
+            //         echo "Tittle: " . $messageText->tittle . "<br>";
+            //         echo "Content:<br>" . $messageText->content . "<br></div>";
+            //     }
+            // }
 
             ?>
     </div>
--- bool/02PHPbasic/php12Post.php
+++ bool/02PHPbasic/php12Post.php
@@ -8,16 +8,40 @@ class Message
 
 $messageTittle = $_POST["tittle"];
 $messageContent = $_POST["content"];
+$timeNow = time();
 
 $message = new Message();
 $message->tittle = $messageTittle;
 $message->content = $messageContent;
 $message->time = "20".date("y/m/d/h:i",time());
-$messageText = json_encode($message);
+//以MySql为载体存储留言数据
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "messageboard";
+$tbname = "message";
 
-$fileMessage = fopen("./msg.txt", 'a');
-fwrite($fileMessage, $messageText . "\n");
-fclose($fileMessage);
+$dbConnect = mysqli_connect($servername,$username,$password);
+
+if(!$dbConnect)
+{
+    echo "无法连接数据库.".mysqli_error();
+    exit();
+}
+
+mysqli_select_db($dbConnect,$dbname);
+$sql = "INSERT INTO $tbname(tittle,content,time) VALUES ('$messageTittle','$messageContent',$timeNow)";
+mysqli_query($dbConnect,$sql);
+
+mysqli_close($dbConnect);
+
+
+//以Json格式写入文件存储留言数据
+// $messageText = json_encode($message);
+// $fileMessage = fopen("./msg.txt", 'a');
+// fwrite($fileMessage, $messageText . "\n");
+// fclose($fileMessage);
+
 header("Location:./php12MessageBoard.php");
 ?>
 <!-- <p>留言发表成功.</p>
