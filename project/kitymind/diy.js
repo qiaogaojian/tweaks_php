bba97f5445bec8a243b2462acc3a61a4d1d48d43
gaojian <qiaogaojian@vip.qq.com>
2018-08-03 17:32:23 +0800

    去功能限制

--- project/kitymind/diy.js
+++ project/kitymind/diy.js
@@ -1,57 +1,57 @@
 (function(){
 	var oldData;
-	var html = '';
-	html += '<a class="diy export" data-type="json">导出json</a>',
-	html += '<a class="diy export" data-type="md">导出md</a>',
-	html += '<a class="diy export" data-type="km">导出km</a>',
-	html += '<button class="diy input">',
-	html += '导入<input type="file" id="fileInput">',
-	html += '</button>';
+	var html  = '';
+	    html += '<a class="diy export" data-type="json">导出json</a>',
+	    html += '<a class="diy export" data-type="md">导出md</a>',
+	    html += '<a class="diy export" data-type="km">导出km</a>',
+	    html += '<button class="diy input">',
+	    html += '导入<input type="file" id="fileInput">',
+	    html += '</button>';
 
 	$('.editor-title').append(html);
 
 	$('.diy').css({
 		// 'height': '30px',
 		// 'line-height': '30px',
-		'margin-top': '0px',
-		'float': 'right',
+		'margin-top'      : '0px',
+		'float'           : 'right',
 		'background-color': '#fff',
-		'min-width': '60px',
-		'text-decoration': 'none',
-		color: '#999',
-		'padding': '0 10px',
-		border: 'none',
-		'border-right': '1px solid #ccc',
+		'min-width'       : '60px',
+		'text-decoration' : 'none',
+		color             : '#999',
+		'padding'         : '0 10px',
+		border            : 'none',
+		'border-right'    : '1px solid #ccc',
 	});
 	$('.input').css({
 		'overflow': 'hidden',
 		'position': 'relative',
 	}).find('input').css({
-		cursor: 'pointer',
+		cursor  : 'pointer',
 		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		left: 0,
-		right: 0,
-		display: 'inline-block',
-		opacity: 0
+		top     : 0,
+		bottom  : 0,
+		left    : 0,
+		right   : 0,
+		display : 'inline-block',
+		opacity : 0
 	});
-	$('.export').css('cursor','not-allowed');
+	$('.export').css('cursor','cursor');
 
 	$(document).on('mouseover', '.export', function(event) {
 		// 链接在hover的时候生成对应数据到链接中
 		event.preventDefault();
 		var $this = $(this),
-				type = $this.data('type'),
+		    type  = $this.data('type'),
 				exportType;
 		switch(type){
-			case 'km':
+			case 'km': 
 				exportType = 'json';
 				break;
-			case 'md':
+			case 'md': 
 				exportType = 'markdown';
 				break;
-			default:
+			default: 
 				exportType = type;
 				break;
 		}
@@ -63,24 +63,24 @@
 
 		editor.minder.exportData(exportType).then(function(content){
 			switch(exportType){
-				case 'json':
+				case 'json': 
 					console.log($.parseJSON(content));
 					break;
-				default:
+				default: 
 					console.log(content);
 					break;
 			}
 			$this.css('cursor', 'pointer');
-			var blob = new Blob([content]),
-					url = URL.createObjectURL(blob);
-			var aLink = $this[0];
-			aLink.href = url;
-			aLink.download = $('#node_text1').text()+'.'+type;
+			var blob           = new Blob([content]),
+			    url            = URL.createObjectURL(blob);
+			var aLink          = $this[0];
+			    aLink.href     = url;
+			    aLink.download = $('#node_text1').text()+'.'+type;
 		});
 	}).on('mouseout', '.export', function(event) {
 		// 鼠标移开是设置禁止点击状态，下次鼠标移入时需重新计算需要生成的文件
 		event.preventDefault();
-		$(this).css('cursor', 'not-allowed');
+		$(this).css('cursor', 'pointer');
 	}).on('click', '.export', function(event) {
 		// 禁止点击状态下取消跳转
 		var $this = $(this);
@@ -99,20 +99,20 @@
 					fileType = file.name.substr(file.name.lastIndexOf('.')+1);
 			console.log(file);
 			switch(fileType){
-				case 'md':
+				case 'md': 
 					fileType = 'markdown';
 					break;
-				case 'km':
-				case 'json':
+				case 'km'  : 
+				case 'json': 
 					fileType = 'json';
 					break;
-				default:
+				default: 
 					console.log("File not supported!");
 					alert('只支持.km、.md、.json文件');
 					return;
 			}
-			var reader = new FileReader();
-			reader.onload = function(e) {
+			var reader        = new FileReader();
+			    reader.onload = function(e) {
 				var content = reader.result;
 				editor.minder.importData(fileType, content).then(function(data){
 					console.log(data)
@@ -122,5 +122,4 @@
 			reader.readAsText(file);
 		});
 	}
-
 })();
