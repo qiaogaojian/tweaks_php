999df96cfec13fbea2ab853ba88700118de3254d
Michael <qiaogaojian@vip.qq.com>
2018-08-07 00:03:42 +0800

    where 子查询

--- learn/105mysql/15_whereziju.sql
+++ learn/105mysql/15_whereziju.sql
@@ -2,3 +2,5 @@
 select * from goods where goods_id = (select max(goods_id) from goods);
 
 select * from goods where goods_id in (select max(goods_id) from goods group by cat_id);
+
+select cat_id, cat_name from category where cat_id = (select cat_id from goods where goods_id=19);
