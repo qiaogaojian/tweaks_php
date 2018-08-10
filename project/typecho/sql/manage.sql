788d24a316b45376b08ed501c87da1555712c200
gaojian <qiaogaojian@vip.qq.com>
2018-08-10 13:52:04 +0800

    typecho 分类表信息更新sql语句

--- project/typecho/sql/manage.sql
+++ project/typecho/sql/manage.sql
@@ -1,9 +1,5 @@
--- 改哪张表 你要改哪几列的值 分别改为什么值 在哪些行生效 (结构)
-update user set age = 18 where name = "Michael";
 
--- 更新多个属性时记得加逗号',' 属性的顺序和表结构保持一致
-update user set name = "迈克尔", age = 19 where uid = 2;
-
+//分类信息
 truncate typecho_metas;
 
 insert into typecho_metas
