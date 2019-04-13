674ca8f456134b91e0a4f5fc8455a88974ff759b
Michael <qiaogaojian@vip.qq.com>
2019-04-14 02:32:24 +0800

    type conversion

--- js/learnjs.js
+++ js/learnjs.js
@@ -16,7 +16,30 @@ It’s just a special value which represents “nothing”, “empty” or “va
 The code above states that age is unknown or empty for some reason.*/
 // alert(typeof(110));
 
-let name = "Ilya";
-alert( `hello ${1}` ); // 1
-alert( `hello ${"name"}` ); // name
-alert( `hello ${name}` ); // Ilya
+// let name = "Ilya";
+// alert( `hello ${1}` ); // 1
+// alert( `hello ${"name"}` ); // name
+// alert( `hello ${name}` ); // Ilya
+
+// alert(1 + "a");
+// alert(1 + "2");
+
+/*undefined is NaN as a number, not 0.
+"0" and space-only strings like " " are true as a boolean.*/
+// alert( Boolean("0") ); // true
+// alert( Boolean(" ") ); // spaces, also true (any non-empty string is true)
+
+alert("" + 1 + 0);      //10
+alert("" - 1 + 0);      //-1
+alert(true + false);    //1
+alert(6 / "3");         //2
+alert("2" * "3");       //23
+alert(4 + 5 + "px");    //45px
+alert("$" + 4 + 5);     //$45
+alert("4" - 2);         //2
+alert("4px" - 2);       //nan
+alert(7 / 0);           //infinate
+alert("  -9  " + 5);    //-95
+alert("  -9  " - 5);    //-14
+alert(null + 1);        //1
+alert(undefined + 1);   //nan
