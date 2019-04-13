d3b18bf548d812353ead1c8b1357e8fd5054e1f4
Michael <qiaogaojian@vip.qq.com>
2019-04-14 02:14:56 +0800

    data types

--- js/learnjs.js
+++ js/learnjs.js
@@ -1,7 +1,22 @@
 "use strict"
 // alert("I'm JavaScript!");
 
-let admin;
-let name = "John";
-admin = name;
-alert(admin);
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
+
+// alert(1/0);
+// let age = 19;
+// alert(`My age is ${age}`);
+
+// let age = null;
+/*In JavaScript, null is not a “reference to a non-existing object” or a “null pointer” like in some other languages.
+It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+The code above states that age is unknown or empty for some reason.*/
+// alert(typeof(110));
+
+let name = "Ilya";
+alert( `hello ${1}` ); // 1
+alert( `hello ${"name"}` ); // name
+alert( `hello ${name}` ); // Ilya
