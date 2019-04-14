99716b06f4d5b569efc19cdd21642e73b7f5fbaf
Michael <qiaogaojian@vip.qq.com>
2019-04-14 22:01:07 +0800

    type convertion

--- js/learnjs.js
+++ js/learnjs.js
@@ -29,17 +29,25 @@ The code above states that age is unknown or empty for some reason.*/
 // alert( Boolean("0") ); // true
 // alert( Boolean(" ") ); // spaces, also true (any non-empty string is true)
 
-alert("" + 1 + 0);      //10
-alert("" - 1 + 0);      //-1
-alert(true + false);    //1
-alert(6 / "3");         //2
-alert("2" * "3");       //23
-alert(4 + 5 + "px");    //45px
-alert("$" + 4 + 5);     //$45
-alert("4" - 2);         //2
-alert("4px" - 2);       //nan
-alert(7 / 0);           //infinate
-alert("  -9  " + 5);    //-95
-alert("  -9  " - 5);    //-14
-alert(null + 1);        //1
-alert(undefined + 1);   //nan
+// alert("" + 1 + 0);      //10
+// alert("" - 1 + 0);      //-1
+// alert(true + false);    //1
+// alert(6 / "3");         //2
+// alert("2" * "3");       //23        6
+// alert(4 + 5 + "px");    //45px      9px
+// alert("$" + 4 + 5);     //$45
+// alert("4" - 2);         //2
+// alert("4px" - 2);       //nan
+// alert(7 / 0);           //infinity
+// alert("  -9  " + 5);    //-95       -9 5
+// alert("  -9  " - 5);    //-14
+// alert(null + 1);        //1
+// alert(undefined + 1);   //nan
+
+let a = 0;
+alert( Boolean(a) ); // false
+
+let b = "0";
+alert( Boolean(b) ); // true
+
+alert(a == b); // true!
