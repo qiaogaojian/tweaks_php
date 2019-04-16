566c8d3252343e8045e6960654f2cc290bb6fad8
qiaogaojian <qiaogaojian@vip.qq.com>
2019-04-16 18:02:46 +0800

    switch

--- js/learnjs.js
+++ js/learnjs.js
@@ -90,10 +90,63 @@ The code above states that age is unknown or empty for some reason.*/
 // }
 
 // let result = (a + b < 4) ? "Below" : "Over";
-let login = prompt("Who are you ?");
-let message =
-    (login == "Employee") ? "Hello" :
-        (login == "Director") ? "Greeting" :
-            (login == "") ? "No login" :
-                "";
-alert(message);
+// let login = prompt("Who are you ?");
+// let message =
+//     (login == "Employee") ? "Hello" :
+//         (login == "Director") ? "Greeting" :
+//             (login == "") ? "No login" :
+//                 "";
+// alert(message);
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
+
+//   if (browse==='Edge') {
+//     alert( "You've got the Edge!" );
+//   }else if(browse==='Chrome'||browse==='Firefox'||browse==='Safari'||browse==='Opera')
+//   {
+//     alert( 'Okay we support these browsers too' );
+//   }else{
+//     alert( 'We hope that this page looks ok!' );
+//   }
+
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+let a = +prompt('a?', '');
+switch (a)
+{
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+}
