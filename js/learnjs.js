1b3688b14b9cd803fcfe2d60082dc0c93b197cc1
qiaogaojian <qiaogaojian@vip.qq.com>
2019-04-25 10:52:49 +0800

    set

--- js/learnjs.js
+++ js/learnjs.js
@@ -624,3 +624,18 @@ usersMapped = [
 // alert(usersMapped[0].id) // 1
 // alert(usersMapped[0].fullName) // John Smith
 
+function unique (arr)
+{
+    let set = new Set();
+    for (let i = 0; i < arr.length; i++)
+    {
+        set.add(arr[i]);
+    }
+    return Array.from(set);
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert(unique(values)); // Hare, Krishna, :-O
