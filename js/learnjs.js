82654391faa0492481e730835bdeb5332e759ee5
qiaogaojian <qiaogaojian@vip.qq.com>
2019-04-25 17:12:17 +0800

    json

--- js/learnjs.js
+++ js/learnjs.js
@@ -817,3 +817,39 @@ usersMapped = [
 //     return (date - start) / 1000;
 // }
 // alert(getSecondsToday(new Date(2019, 4, 25, 10)));
+
+// let user = {
+//     name: "John Smith",
+//     age: 35
+// };
+// let json = JSON.stringify(user);
+// alert(json);
+// let userBack = JSON.parse(json);
+// alert(userBack.name);
+
+// let room = {
+//     number: 23
+// };
+
+// let meetup = {
+//     title: "Conference",
+//     occupiedBy: [{ name: "John" }, { name: "Alice" }],
+//     place: room
+// };
+
+// // circular references
+// room.occupiedBy = meetup;
+// meetup.self = meetup;
+
+// alert(JSON.stringify(meetup, function replacer (key, value)
+// {
+//     return (key != "" && value == meetup) ? undefined : value;
+// }));
+
+/* result should be:
+{
+  "title":"Conference",
+  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
+  "place":{"number":23}
+}
+*/
