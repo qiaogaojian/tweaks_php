c998107fd338f87cd8ff24fc97ebdeaefe52e5ba
qiaogaojian <qiaogaojian@vip.qq.com>
2019-04-29 16:25:26 +0800

    prototypes

--- js/learnjs.js
+++ js/learnjs.js
@@ -1232,3 +1232,23 @@ usersMapped = [
 // let michael = new User("Michael",new Date(1992,5,4));
 // alert(michael.name);
 // alert(michael.age);
+
+// let head = {
+//     glasses: 1
+// };
+
+// let table = {
+//     __proto__: head,
+//     pen: 3
+// };
+
+// let bed = {
+//     __proto__: table,
+//     sheet: 1,
+//     pillow: 2
+// };
+
+// let pockets = {
+//     __proto__: bed,
+//     money: 2000
+// };
