3c3e606f7050eca78a2dcecfdcfe46ecab44a7c8
qiaogaojian <qiaogaojian@vip.qq.com>
2019-04-30 18:02:34 +0800

    class

--- js/learnjs.js
+++ js/learnjs.js
@@ -1252,3 +1252,20 @@ usersMapped = [
 //     __proto__: bed,
 //     money: 2000
 // };
+
+class User
+{
+    constructor(name)
+    {
+        this.name = name;
+    }
+
+    sayHi ()
+    {
+        alert(this.name);
+    }
+}
+
+// Usage:
+let user = new User("John");
+user.sayHi();
