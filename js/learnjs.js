76745108cf3ed9ffee869d4ca620313d5712685e
qiaogaojian <qiaogaojian@vip.qq.com>
2019-05-06 17:51:50 +0800

    Static properties and methods

--- js/learnjs.js
+++ js/learnjs.js
@@ -1351,21 +1351,55 @@ usersMapped = [
 //   rabbit.run(5); // White Rabbit runs with speed 5.
 //   rabbit.stop(); // White Rabbit stopped. White rabbit hides!
 
-class Animal
-{
-    constructor(name)
-    {
-        this.name = name;
+// class Animal
+// {
+//     constructor(name)
+//     {
+//         this.name = name;
+//     }
+// }
+// class Rabbit extends Animal
+// {
+//     constructor(name)
+//     {
+//         super(name);
+//         this.name = name;
+//         this.created = Date.now();
+//     }
+// }
+// let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+// alert(rabbit.name);
+
+class Animal {
+
+    constructor(name, speed) {
+      this.speed = speed;
+      this.name = name;
     }
-}
-class Rabbit extends Animal
-{
-    constructor(name)
-    {
-        super(name);
-        this.name = name;
-        this.created = Date.now();
+
+    run(speed = 0) {
+      this.speed += speed;
+      alert(`${this.name} runs with speed ${this.speed}.`);
     }
-}
-let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
-alert(rabbit.name);
+
+    static compare(animalA, animalB) {
+      return animalA.speed - animalB.speed;
+    }
+
+  }
+
+  // Inherit from Animal
+  class Rabbit extends Animal {
+    hide() {
+      alert(`${this.name} hides!`);
+    }
+  }
+
+  let rabbits = [
+    new Rabbit("White Rabbit", 10),
+    new Rabbit("Black Rabbit", 5)
+  ];
+
+  rabbits.sort(Rabbit.compare);
+
+  rabbits[0].run(); // Black Rabbit runs with speed 5.
