f56d8ad7aae44500e6e1ade8a317757fc3aea5bc
qiaogaojian <qiaogaojian@vip.qq.com>
2019-05-06 18:09:41 +0800

    Static properties and methods

--- js/learnjs.js
+++ js/learnjs.js
@@ -1372,34 +1372,34 @@ usersMapped = [
 
 class Animal {
 
-    constructor(name, speed) {
-      this.speed = speed;
-      this.name = name;
-    }
-
-    run(speed = 0) {
-      this.speed += speed;
-      alert(`${this.name} runs with speed ${this.speed}.`);
-    }
-
-    static compare(animalA, animalB) {
-      return animalA.speed - animalB.speed;
-    }
-
+  constructor(name, speed) {
+    this.speed = speed;
+    this.name = name;
   }
 
-  // Inherit from Animal
-  class Rabbit extends Animal {
-    hide() {
-      alert(`${this.name} hides!`);
-    }
+  run(speed = 0) {
+    this.speed += speed;
+    alert(`${this.name} runs with speed ${this.speed}.`);
   }
 
-  let rabbits = [
-    new Rabbit("White Rabbit", 10),
-    new Rabbit("Black Rabbit", 5)
-  ];
+  static compare(animalA, animalB) {
+    return animalA.speed - animalB.speed;
+  }
 
-  rabbits.sort(Rabbit.compare);
+}
 
-  rabbits[0].run(); // Black Rabbit runs with speed 5.
+// Inherit from Animal
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} hides!`);
+  }
+}
+
+let rabbits = [
+  new Rabbit("White Rabbit", 10),
+  new Rabbit("Black Rabbit", 5)
+];
+
+rabbits.sort(Rabbit.compare);
+
+rabbits[0].run(); // Black Rabbit runs with speed 5.
