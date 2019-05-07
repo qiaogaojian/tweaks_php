35a03ece7095c86a2f11153053a262068d132b07
qiaogaojian <qiaogaojian@vip.qq.com>
2019-05-07 16:38:17 +0800

    custom error

--- js/learnjs.js
+++ js/learnjs.js
@@ -1471,69 +1471,87 @@ usersMapped = [
 // machine.waterAmount = 666;
 // alert(machine.#waterAmount); // Error
 
-let eventMixin = {
-    /**
-     * Subscribe to event, usage:
-     *  menu.on('select', function(item) { ... }
-    */
-    on (eventName, handler)
-    {
-        if (!this._eventHandlers) this._eventHandlers = {};
-        if (!this._eventHandlers[eventName])
-        {
-            this._eventHandlers[eventName] = [];
-        }
-        this._eventHandlers[eventName].push(handler);
-    },
+// let eventMixin = {
+//     /**
+//      * Subscribe to event, usage:
+//      *  menu.on('select', function(item) { ... }
+//     */
+//     on (eventName, handler)
+//     {
+//         if (!this._eventHandlers) this._eventHandlers = {};
+//         if (!this._eventHandlers[eventName])
+//         {
+//             this._eventHandlers[eventName] = [];
+//         }
+//         this._eventHandlers[eventName].push(handler);
+//     },
 
-    /**
-     * Cancel the subscription, usage:
-     *  menu.off('select', handler)
-     */
-    off (eventName, handler)
-    {
-        let handlers = this._eventHandlers && this._eventHandlers[eventName];
-        if (!handlers) return;
-        for (let i = 0; i < handlers.length; i++)
-        {
-            if (handlers[i] === handler)
-            {
-                handlers.splice(i--, 1);
-            }
-        }
-    },
+//     /**
+//      * Cancel the subscription, usage:
+//      *  menu.off('select', handler)
+//      */
+//     off (eventName, handler)
+//     {
+//         let handlers = this._eventHandlers && this._eventHandlers[eventName];
+//         if (!handlers) return;
+//         for (let i = 0; i < handlers.length; i++)
+//         {
+//             if (handlers[i] === handler)
+//             {
+//                 handlers.splice(i--, 1);
+//             }
+//         }
+//     },
 
-    /**
-     * Generate the event and attach the data to it
-     *  this.trigger('select', data1, data2);
-     */
-    trigger (eventName, ...args)
-    {
-        if (!this._eventHandlers || !this._eventHandlers[eventName])
-        {
-            return; // no handlers for that event name
-        }
+//     /**
+//      * Generate the event and attach the data to it
+//      *  this.trigger('select', data1, data2);
+//      */
+//     trigger (eventName, ...args)
+//     {
+//         if (!this._eventHandlers || !this._eventHandlers[eventName])
+//         {
+//             return; // no handlers for that event name
+//         }
 
-        // call the handlers
-        this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
-    }
-};
+//         // call the handlers
+//         this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+//     }
+// };
 
-// Make a class
-class Menu
+// // Make a class
+// class Menu
+// {
+//     choose (value)
+//     {
+//         this.trigger("select", value);
+//     }
+// }
+// // Add the mixin
+// Object.assign(Menu.prototype, eventMixin);
+
+// let menu = new Menu();
+
+// // call the handler on selection:
+// menu.on("select", value => alert(`Value selected: ${value}`));
+
+// // triggers the event => shows Value selected: 123
+// menu.choose("123"); // value selected
+
+class FormatError extends SyntaxError
 {
-    choose (value)
+    constructor(msg)
     {
-        this.trigger("select", value);
+        super(msg);
+        this.name = this.constructor.name;
     }
 }
-// Add the mixin
-Object.assign(Menu.prototype, eventMixin);
 
-let menu = new Menu();
+let err = new FormatError("formatting error");
 
-// call the handler on selection:
-menu.on("select", value => alert(`Value selected: ${value}`));
+alert( err.message ); // formatting error
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
 
-// triggers the event => shows Value selected: 123
-menu.choose("123"); // value selected
+alert( err instanceof FormatError ); // true
+alert( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
