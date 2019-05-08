441b3ed55a92f1c6db12f9801d8b4d765393f1bc
qiaogaojian <qiaogaojian@vip.qq.com>
2019-05-08 16:19:55 +0800

    async await error

--- js/learnjs.js
+++ js/learnjs.js
@@ -1668,3 +1668,28 @@ usersMapped = [
 
 // showAvatar();
 // alert("after await");
+
+// async function loadJson (url)
+// {
+//     let response = await fetch(url);
+
+//     if (response.status == 200) {
+//         let json = await response.json();
+//         return json;
+//     }
+//     throw new Error(response.status);
+//     // await fetch(url)
+//     //     .then(response =>
+//     //     {
+//     //         if (response.status == 200)
+//     //         {
+//     //             return response.json();
+//     //         } else
+//     //         {
+//     //             throw new Error(response.status);
+//     //         }
+//     //     })
+// }
+
+// loadJson('https://no-such-user.json') // (3)
+//     .catch(alert); // Error: 404
